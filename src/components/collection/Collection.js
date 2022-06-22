3626785226656fccaa3b14b1fb4bd3469561e55e
realhardworkingdeveloper <realhardworkingdeveloper@gmail.com>
2022-06-22 05:20:46 -0700

    update

--- src/components/collection/Collection.js
+++ src/components/collection/Collection.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import searchIcon from '../../assets/search.png'
 import waterAvatar from '../../assets/water.png'
 import greenAvatar from '../../assets/earth.png'
@@ -6,95 +6,154 @@ import fireAvatar from '../../assets/fire.png'
 import windAvatar from '../../assets/wind.png'
 import soulAvatar from '../../assets/soul.png'
 
+import {
+    refreshAccount,
+    sendTransactions,
+    useGetAccountInfo,
+    useGetNetworkConfig,
+    useGetPendingTransactions,
+} from '@elrondnetwork/dapp-core';
+
+import {
+    GATEWAY,
+    TOKEN_IDS
+} from '../../config';
+
+import axios from 'axios';
 
 function Collection(props) {
+    const { address } = useGetAccountInfo();
+    const { hasPendingTransactions } = useGetPendingTransactions();
+
+    const [ownedNfts, setOwnedNfts] = useState([]);
+
+    useEffect(() => {
+        if(hasPendingTransactions || !address) return;
+        axios
+          .get(`${GATEWAY}/accounts/${address}/nfts?collections=${TOKEN_IDS}`)
+          .then((res) => {
+    
+            const nfts = [];
+    
+            res.data.map(nft => {
+                nfts.push({
+                    id: nft.identifier,
+                    collection: nft.collection,
+                    nonce: nft.nonce,
+                    src: nft.url,
+                    rarity: nft.metadata.rarity === undefined ? 0 : nft.metadata.rarity.rarityScore,
+                    name: nft.name,
+                })
+            });
+    
+            setOwnedNfts(nfts)
+          }).catch(err => {
+            console.log(err);
+          });
+      }, [hasPendingTransactions, TOKEN_IDS, address]);
+
     return (
         <React.Fragment>
             <div className="main-body-wrapper">
                 <h1 className="collection-heading" style={{ marginTop: '2rem' }}>MY NFTS COLLECTION</h1>
-                {/* <p className="collection-para" style={{ marginTop: 10 }}>
-                    Here you will find the number of NFTs we have generated for you,<br />
-                    so you can engage with our platform</p> */}
-                {/* <div style={{ position: 'relative', width: '70%', height: "52px", marginTop: '4rem' }}>
-                    <label className="collection-icon" htmlFor="search" style={{ position: 'absolute' }}>
-                        <img src={searchIcon} style={{ objectFit: 'contain' }} width='100%' height='100%' />
-                    </label>
-                    <input defaultValue={'Search Nfts collection'} placeholder="Search Nfts collection" id="search" className="collection-search" />
-                </div> */}
-                <div className="collection-wrapper">
-                    <div
-                        // onClick={() => window.location.href = '/attributes'}
-                        className="collection-card">
-                        <div className="collection-card-cover"></div>
-                        <img src={waterAvatar} style={{ objectFit: 'contain', width: "25rem", minWidth: "100%", height: "340px", marginBottom: '1rem' }} />
-                        <button className="collection-button">
-                            Enki
-                        </button>
-                    </div>
-                    <div className="collection-card"
-                    // onClick={() => window.location.href = '/attributes'}
-                    >
-                        <div className="collection-card-cover"></div>
-                        <img src={greenAvatar} style={{ objectFit: 'contain', width: "25rem", minWidth: "100%", height: "340px", marginBottom: '1rem' }} />
-                        <button className="collection-button">
-                            Finlandez
-                        </button>
-                    </div>
-                    <div className="collection-card"
-                    // onClick={() => window.location.href = '/attributes'}
-                    >
-                        <div className="collection-card-cover"></div>
-                        <img src={waterAvatar} style={{ objectFit: 'contain', width: "25rem", minWidth: "100%", height: "340px", marginBottom: '1rem' }} />
-                        <button className="collection-button">
-                            Enki
-                        </button>
-                    </div>
-                    <div className="collection-card"
-                    // onClick={() => window.location.href = '/attributes'}
-                    >
-                        <div className="collection-card-cover"></div>
-                        <img src={greenAvatar} style={{ objectFit: 'contain', width: "25rem", minWidth: "100%", height: "340px", marginBottom: '1rem' }} />
-                        <button className="collection-button">
-                            Finlandez
-                        </button>
-                    </div>
-                    <div className="collection-card"
-                    // onClick={() => window.location.href = '/attributes'}
-                    >
-                        <div className="collection-card-cover"></div>
-                        <img src={fireAvatar} style={{ objectFit: 'contain', width: "25rem", minWidth: "100%", height: "340px", marginBottom: '1rem' }} />
-                        <button className="collection-button">
-                            Chimera
-                        </button>
-                    </div>
-                    <div className="collection-card"
-                    // onClick={() => window.location.href = '/attributes'}
-                    >
-                        <div className="collection-card-cover"></div>
-                        <img src={soulAvatar} style={{ objectFit: 'contain', width: "25rem", minWidth: "100%", height: "340px", marginBottom: '1rem' }} />
-                        <button className="collection-button">
-                            Eleos
-                        </button>
-                    </div>
-                    <div className="collection-card"
-                    // onClick={() => window.location.href = '/attributes'}
-                    >
-                        <div className="collection-card-cover"></div>
-                        <img src={fireAvatar} style={{ objectFit: 'contain', width: "25rem", minWidth: "100%", height: "340px", marginBottom: '1rem' }} />
-                        <button className="collection-button">
-                            Chimera
-                        </button>
-                    </div>
-                    <div className="collection-card"
-                    // onClick={() => window.location.href = '/attributes'}
-                    >
-                        <div className="collection-card-cover"></div>
-                        <img src={soulAvatar} style={{ objectFit: 'contain', width: "25rem", minWidth: "100%", height: "340px", marginBottom: '1rem' }} />
-                        <button className="collection-button">
-                            Eleos
-                        </button>
-                    </div>
-                </div>
+
+                
+                    {
+                        ownedNfts.length === 0 ? (
+                            <div className="collection-wrapper">
+                                <div
+                                    // onClick={() => window.location.href = '/attributes'}
+                                    className="collection-card">
+                                    <div className="collection-card-cover"></div>
+                                    <img src={waterAvatar} style={{ objectFit: 'contain', width: "25rem", minWidth: "100%", height: "340px", marginBottom: '1rem' }} />
+                                    <button className="collection-button">
+                                        Enki
+                                    </button>
+                                </div>
+                                <div className="collection-card"
+                                // onClick={() => window.location.href = '/attributes'}
+                                >
+                                    <div className="collection-card-cover"></div>
+                                    <img src={greenAvatar} style={{ objectFit: 'contain', width: "25rem", minWidth: "100%", height: "340px", marginBottom: '1rem' }} />
+                                    <button className="collection-button">
+                                        Finlandez
+                                    </button>
+                                </div>
+                                <div className="collection-card"
+                                // onClick={() => window.location.href = '/attributes'}
+                                >
+                                    <div className="collection-card-cover"></div>
+                                    <img src={waterAvatar} style={{ objectFit: 'contain', width: "25rem", minWidth: "100%", height: "340px", marginBottom: '1rem' }} />
+                                    <button className="collection-button">
+                                        Enki
+                                    </button>
+                                </div>
+                                <div className="collection-card"
+                                // onClick={() => window.location.href = '/attributes'}
+                                >
+                                    <div className="collection-card-cover"></div>
+                                    <img src={greenAvatar} style={{ objectFit: 'contain', width: "25rem", minWidth: "100%", height: "340px", marginBottom: '1rem' }} />
+                                    <button className="collection-button">
+                                        Finlandez
+                                    </button>
+                                </div>
+                                <div className="collection-card"
+                                // onClick={() => window.location.href = '/attributes'}
+                                >
+                                    <div className="collection-card-cover"></div>
+                                    <img src={fireAvatar} style={{ objectFit: 'contain', width: "25rem", minWidth: "100%", height: "340px", marginBottom: '1rem' }} />
+                                    <button className="collection-button">
+                                        Chimera
+                                    </button>
+                                </div>
+                                <div className="collection-card"
+                                // onClick={() => window.location.href = '/attributes'}
+                                >
+                                    <div className="collection-card-cover"></div>
+                                    <img src={soulAvatar} style={{ objectFit: 'contain', width: "25rem", minWidth: "100%", height: "340px", marginBottom: '1rem' }} />
+                                    <button className="collection-button">
+                                        Eleos
+                                    </button>
+                                </div>
+                                <div className="collection-card"
+                                // onClick={() => window.location.href = '/attributes'}
+                                >
+                                    <div className="collection-card-cover"></div>
+                                    <img src={fireAvatar} style={{ objectFit: 'contain', width: "25rem", minWidth: "100%", height: "340px", marginBottom: '1rem' }} />
+                                    <button className="collection-button">
+                                        Chimera
+                                    </button>
+                                </div>
+                                <div className="collection-card"
+                                // onClick={() => window.location.href = '/attributes'}
+                                >
+                                    <div className="collection-card-cover"></div>
+                                    <img src={soulAvatar} style={{ objectFit: 'contain', width: "25rem", minWidth: "100%", height: "340px", marginBottom: '1rem' }} />
+                                    <button className="collection-button">
+                                        Eleos
+                                    </button>
+                                </div>
+                            </div>
+                        ) : (
+                            <div className="collection-wrapper">
+                                {ownedNfts.map(nft => (
+                                    <div 
+                                        className="collection-card"
+                                        key={nft.id}
+                                    // onClick={() => window.location.href = '/attributes'}
+                                    >
+                                        {/* <div className="collection-card-cover"></div> */}
+                                        <img src={nft.src} style={{ objectFit: 'contain', width: "25rem", minWidth: "100%", height: "340px", marginBottom: '1rem' }} />
+                                        <button className="collection-button">
+                                            {nft.name}
+                                        </button>
+                                    </div>
+                                ))}
+                            </div>
+                        )
+                    }
+                    
+                
             </div>
         </React.Fragment>
     );
--- src/config.js
+++ src/config.js
@@ -10,3 +10,4 @@ export const TIMEOUT = 6000;
 // export const NETWORK = 'devnet';
 export const GATEWAY = 'https://devnet-api.elrond.com';
 
+export const TOKEN_IDS = "CRB-6de881,CRB-82a656,CRB-a8f4f9,CRB-d9a008,CRB-e54100";
